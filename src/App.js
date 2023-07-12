ba4c37ef036e832f3f72c4dac8e342d0bbbb7879
dhananjayaaps <dhananjayaaps@gmail.com>
2023-07-13 01:01:48 +0530

    'first commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+          <div className="form-wrapper">
+              <div className="banner">
+                  <h1>Hello, Friend!</h1>
+                  <p>Enter your personal details and start journey with us</p>
+              </div>
+              <div className="green-bg">
+                  <button type="button">Sign Up</button>
+              </div>
+              <form className="signup-form">
+                  <h1>Create Account</h1>
+                  <div className="social-media">
+                      <i className="fab fa-facebook-f"></i>
+                      <i className="fab fa-instagram"></i>
+                      <i className="fab fa-linkedin-in"></i>
+                  </div>
+                  <p>or use your email for registration</p>
+                  <div className="input-group">
+                      <i className="fas fa-user"></i>
+                      <input type="text" placeholder="Name"/>
+                  </div>
+                  <div className="input-group">
+                      <i className="fas fa-envelope"></i>
+                      <input type="email" placeholder="Email"/>
+                  </div>
+                  <div className="input-group">
+                      <i className="fas fa-lock"></i>
+                      <input type="password" placeholder="Password"/>
+                  </div>
+                  <button type="button">Sign Up</button>
+              </form>
+          </div>
+      </div>
   );
+
+    const container = document.querySelector(".container");
+    const signUpBtn = document.querySelector(".green-bg button");
+
+    signUpBtn.addEventListener("click", () => {
+        container.classList.toggle("change");
+    });
 }
 
 export default App;
